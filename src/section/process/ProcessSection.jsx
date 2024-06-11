e0baa1cf06b998d4f57984a7182eef22a7b366b3
manish45-comp <manishbagal45@gmail.com>
2024-06-11 21:46:35 +0530

    minor

--- src/section/process/ProcessSection.jsx
+++ src/section/process/ProcessSection.jsx
@@ -3,7 +3,7 @@ function ProcessSection() {
     <div className="process rounded-3xl mt-10 p-5">
       <div className="grid grid-cols-1 lg:grid-cols-25 grid-rows-2 gap-10 my-10 md:my-20">
         <div className="lg:col-start-4 lg:col-end-13 span-1">
-          <h1 className="id text-3xl opa1city-50">
+          <h1 className="id text-3xl opacity-50">
             <span>01</span>
           </h1>
           <h3 className="subtitle-secondary my-4">
@@ -20,7 +20,7 @@ function ProcessSection() {
         </div>
 
         <div className="span-1 lg:col-start-14 lg:col-end-23">
-          <h1 className="id text-3xl opa1city-50">
+          <h1 className="id text-3xl opacity-50">
             <span>02</span>
           </h1>
           <h3 className="subtitle-secondary my-4">
@@ -37,7 +37,7 @@ function ProcessSection() {
         </div>
 
         <div className="lg:col-start-4 lg:col-end-13 span-1 ">
-          <h1 className="id text-3xl opa1city-50">
+          <h1 className="id text-3xl opacity-50">
             <span>03</span>
           </h1>
           <h3 className="subtitle-secondary my-4">
@@ -52,7 +52,7 @@ function ProcessSection() {
           </p>
         </div>
         <div className="span-1 lg:col-start-14 lg:col-end-23">
-          <h1 className="id text-3xl opa1city-50">
+          <h1 className="id text-3xl opacity-50">
             <span>04</span>
           </h1>
           <h3 className="subtitle-secondary my-4">
